c79a64aad164f810677bf8954066a68c696cd764
wherevlad <where.vlad@gmail.com>
2017-04-22 18:01:35 +0700

    ver 1.1 добавлен шаблон для программы

--- main.cpp
+++ main.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
+void PrintMas(int **a, int n, int m);			//функция вывода массива
 
 
-int main(){
 
-	cout << "Hello World!" << endl;
+int main()
+{
+	srand ( time(NULL) ); 
+    int n = 10, m = 10; 
+    int mas_test[10][10] = {					//тестовый массив на время, пока не создадим генератор лабиринта
+		-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		-1,  0, -1, -1, -1, -1, -1, -1, -1,  0,
+		-1,  0, -1,  0,  0,  0, -1,  0,  0,  0,
+		-1,  0, -1,  0, -1,  0, -1,  0, -1, -1,
+		-1,  0, -1,  0, -1,  0, -1,  0,  0,  0,
+		-1,  0, -1, -1, -1,  0, -1, -1, -1,  0,
+		-1,  0,  0,  0,  0,  0, -1,  0,  0,  0,
+		-1, -1, -1, -1, -1, -1, -1,  0, -1, -1,
+		-1,  0,  0,  0,  0,  0,  0,  0, -1, -1 
+		};
 
+    int **a = new int *[n]; // Выделение памяти для массива
+    
+    for (int i=0; i<n; i++)
+         a[i] = new int [m];
+         
+
+    for (int i=0; i<n; i++){
+         for (int j=0; j<m; j++){
+            a[i][j] = mas_test[i][j];
+         }
+    }
+    
+    PrintMas(a, n, m);
+
+    
+    delete [] a; // очистка памяти
     return 0;
+}
+
+
+void PrintMas(int **a, int n, int m){			//функция вывода массива
+	cout << endl;
+    for (int i=0; i<n; i++){
+         for (int j=0; j<m; j++){
+            if(a[i][j] == -1) cout << "8 ";
+            else cout << "  ";
+         }
+         cout << endl;
+    }
+    cout << endl;
 }
