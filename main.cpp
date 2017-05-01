03c6b1d67c20805bd2a36635cb21b7fcf88f5c03
KirzakCib <Kirzak899@gmail.com>
2017-05-01 20:22:54 +0300

    Finf

--- main.cpp
+++ main.cpp
@@ -1,58 +1,109 @@
+#include "conio.h"   
+#include <string.h>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
-void PrintMas(int **a, int n, int m);			//—Ñ—É–Ω–∫—Ü–∏—è –≤—ã–≤–æ–¥–∞ –º–∞—Å—Å–∏–≤–∞
+int a;
+
+int bufp,bufe;
+struct{
+ signed char n,m;
+}buf[256];
 
 
-
-int main()
-{
-	srand ( time(NULL) ); 
-    int n = 10, m = 10; 
-    int mas_test[10][10] = {					//—Ç–µ—Å—Ç–æ–≤—ã–π –º–∞—Å—Å–∏–≤ –Ω–∞ –≤—Ä–µ–º—è, –ø–æ–∫–∞ –Ω–µ —Å–æ–∑–¥–∞–¥–∏–º –≥–µ–Ω–µ—Ä–∞—Ç–æ—Ä –ª–∞–±–∏—Ä–∏–Ω—Ç–∞
-		-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-		-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-		-1,  0, -1, -1, -1, -1, -1, -1, -1,  0,
-		-1,  0, -1,  0,  0,  0, -1,  0,  0,  0,
-		-1,  0, -1,  0, -1,  0, -1,  0, -1, -1,
-		-1,  0, -1,  0, -1,  0, -1,  0,  0,  0,
-		-1,  0, -1, -1, -1,  0, -1, -1, -1,  0,
-		-1,  0,  0,  0,  0,  0, -1,  0,  0,  0,
-		-1, -1, -1, -1, -1, -1, -1,  0, -1, -1,
-		-1,  0,  0,  0,  0,  0,  0,  0, -1, -1 
-		};
-
-    int **a = new int *[n]; // –í—ã–¥–µ–ª–µ–Ω–∏–µ –ø–∞–º—è—Ç–∏ –¥–ª—è –º–∞—Å—Å–∏–≤–∞
-    
-    for (int i=0; i<n; i++)
-         a[i] = new int [m];
-         
-
-    for (int i=0; i<n; i++){
-         for (int j=0; j<m; j++){
-            a[i][j] = mas_test[i][j];
-         }
-    }
-    
-    PrintMas(a, n, m);
-
-    
-    delete [] a; // –æ—á–∏—Å—Ç–∫–∞ –ø–∞–º—è—Ç–∏
-    return 0;
-}
-
-
-void PrintMas(int **a, int n, int m){			//—Ñ—É–Ω–∫—Ü–∏—è –≤—ã–≤–æ–¥–∞ –º–∞—Å—Å–∏–≤–∞
+void PrintMas(int **c, int n, int m){			
 	cout << endl;
     for (int i=0; i<n; i++){
          for (int j=0; j<m; j++){
-            if(a[i][j] == -1) cout << "8 ";
+            if(c[i][j] == -1) cout << "8 ";
             else cout << "  ";
          }
          cout << endl;
     }
     cout << endl;
+}
+
+void Short(int n,int m,int s){
+	int buf;
+ if(c[n][m]<=s)return; 
+ c[n][m]=s;   
+ //buf[bufe].n=n;    
+ //buf[bufe].m=m;    
+ //bufe++; 
+ getch();   
+}
+
+int Way(int *n,int *m){
+ if(bufp==bufe)return 0;
+ *n=buf[bufp].n;
+ *m=buf[bufp].m;
+ bufp++;   
+ return 1;
+}
+
+
+void track(int a,int b,int i,int j){
+//	_fmemset(trackmax,0xFF,sizeof(trackmax));
+	//trackmax Á‡ÔÓÎÌËÚ¸ Ï‡ÍÒËÏ‡Î¸Ì˚ÏË ÁÌ‡˜ÂÌËˇÏË
+ int s,t,n,m; 
+ bufp=bufe=0;    
+ Short(a,b,0);   
+ while(Way(&n,&m)){   
+  if((a==i)&&(b==j)){
+
+  }
+  n=c[i][j]+c[n][m];//ÔÂÂ·Ó ÒÓÒÂ‰ÌËı ÍÎÂÚÓÍ
+  
+  if(c[m+1][n])Short(n,m+1,s); 
+  if(c[m-1][n])Short(n,m-1,s);  
+  if(c[m][n+1])Short(n+1,m,s); 
+  if(c[m][n-1])Short(n-1,m,s); 
+ }
+   if(c[i][j]==-1){
+  cout<<"œÛÚË ÌÂ ÒÛ˘ÂÒÚ‚ÛÂÚ"<<endl;//ƒÓ·‡‚ËÚ¸ Á‡ÎË‚ÍÛ
+  return;
+ }  else 
+ cout<<"œÛÚ¸"<<endl;//ƒÓ·‡‚ËÚ¸ Á‡ÎË‚ÍÛ
+
+ while((n!=a)||(m!=b)){  
+ //ƒÓ·‡‚ËÚ¸ ÔÓËÒÍ ÒÓÒÂ‰ÌÂÈ ÍÎÂÚÍË
+ 
+  if(c[m+1][n]<s){a=n;b=m+1;t=c[m+1][n];} 
+  
+  if(c[m-1][n]<s){a=n;b=m-1;t=c[m-1][n];} 
+  
+  if(c[m][n+1]<s){a=n+1;b=m;t=c[m][n+1];}
+  if(c[m][n-1]<s){a=n-1;b=m;t=c[m][n-1];}
+  n=a;m=b;s=t; 
+
+  getch();   
+ }
+}
+
+int main(){
+ int s, a ,b, i, j,n,m,c;
+ a=1;b=1;   // H‡˜‡Î¸Ì‡ˇ ÚÓ˜Í‡
+ i=3;j=3;  // ÷ÂÎ¸ ÔÛÚË
+
+ int **c = new int *[n];
+PrintMas(c, n, m);
+ track(a,b,i,j); 
+ 
+//    
+//    for (int i=0; i<n; i++)
+//         a[i] = new int [m];
+//         
+//
+//    for (int i=0; i<n; i++){
+//         for (int j=0; j<m; j++){
+//            a[i][j] = mas_test[i][j];
+//         }
+//    }
+    
+
+    
+    delete [] c; 
+    return 0;
 }
