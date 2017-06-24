261b9d7fce89c5563641fd9ce72961bd5976a4f2
KirzakCib <ip615s05@awp11.csc.local>
2017-06-24 10:19:34 +0700

    fix test

--- test/validation_test.cpp
+++ test/validation_test.cpp
@@ -5,17 +5,23 @@
 
 CTEST(main1, main1_test)
 {
-	int expected1 = 0;
+	int expected1 = 8;
 	int expected2 = 20;
-	int result = rand() %40 + 20;
-	ASSERT_INTERVAL(expected1,expected2,result);
-
+	int result = rand() %12 + 8;
 	ASSERT_INTERVAL(expected1,expected2,result);
 }
 
+CTEST(main2, main1_test)
+{
+	int expected1 = 8;
+	int expected2 = 20;
+	int result = 30;
+	ASSERT_INTERVAL(expected1,expected2,result);
+}
+/*
 CTEST(main2, main2_test)
 {
-	/*int a[20][20];
+	int a[20][20];
 	int b[n][m];
 
 	Maze(b, n, m);
@@ -31,5 +37,5 @@ CTEST(main2, main2_test)
 	int a[1] = {0};
 	int b[1] = {1};
 	int n = 1;
-	ASSERT_DATA(a, n, b, n);*/
-}
+	ASSERT_DATA(a, n, b, n);
+}*/
